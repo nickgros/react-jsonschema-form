742ab29611105c4268fd795908573a0c75dd8e74
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-07-22 12:15:54 -0400

    Fix types

--- packages/core/index.d.ts
+++ packages/core/index.d.ts
@@ -437,9 +437,9 @@ declare module '@sage-bionetworks/rjsf-core' {
     }
 }
 
-declare module '@rjsf/core/lib/components/fields/SchemaField' {
+declare module '@sage-bionetworks/rjsf-core/lib/components/fields/SchemaField' {
     import { JSONSchema7 } from 'json-schema';
-    import { FieldProps, UiSchema, IdSchema, FormValidation } from '@rjsf/core';
+    import { FieldProps, UiSchema, IdSchema, FormValidation } from '@sage-bionetworks/rjsf-core';
 
     export type SchemaFieldProps<T = any> = Pick<
         FieldProps<T>,
@@ -449,9 +449,9 @@ declare module '@rjsf/core/lib/components/fields/SchemaField' {
     export default class SchemaField extends React.Component<SchemaFieldProps> {}
 }
 
-declare module '@rjsf/core/lib/validate' {
+declare module '@sage-bionetworks/rjsf-core/lib/validate' {
     import { JSONSchema7Definition } from 'json-schema';
-    import { AjvError, ErrorSchema, FormProps } from '@rjsf/core';
+    import { AjvError, ErrorSchema, FormProps } from '@sage-bionetworks/rjsf-core';
 
     export default function validateFormData<T = any>(
         formData: T,
